e90af7d91ce3f98caee448164378eb0a6eb251a8
projectmiluju <dnjsdyd0712@naver.com>
2024-04-21 03:42:14 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [coords, saveCoords] = useState();
+  const [temp, setTemp] = useState();
+  const [weather, setWeather] = useState();
+  const [cityname, setCityname] = useState();
+      
+     function handleGeoSucc(position) {
+      console.log(position);
+      const latitude = position.coords.latitude;  // 경도  
+      const longitude = position.coords.longitude;  // 위도
+      const coordsObj = {
+        latitude,
+        longitude
+      }
+      saveCoords(coordsObj);
+      getWeather(latitude, longitude);
+    }
+  
+    function handleGeoErr(err) {
+      console.log("geo err! " + err);
+    }
+  
+    function requestCoords() {
+      navigator.geolocation.getCurrentPosition(handleGeoSucc, handleGeoErr);
+    }
+  
+    function getWeather(lat, lon) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3d83df3698d84d54a78013b6a9755c2a&units=metric`)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          const temp = data.main.temp;
+          const weathers = data.weather[data.weather.length - 1];
+          const cityname = data.name;
+          setTemp(temp);
+          setWeather(weathers.main);
+          setCityname(cityname);
+        })
+    }
+  
+    useEffect(() => {
+      requestCoords();
+    }, []);
+
+    return (
+              <>
+                  <p>React기반 날씨 데이터 연동 어플리케이션</p>
+                  <hr/>
+                  <button onClick={requestCoords}>서버에서 날씨 데이터 가져오기</button>
+                  <p>날씨 목록</p>
+                  <li>도시 : {cityname}</li>
+                  <li>온도 : {temp}</li>
+                  <li>날씨 : {weather}</li>
+              </> 
+          )
+}
